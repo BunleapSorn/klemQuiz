71396059b66a36e5e1b83f37997d018bb765c5e1
Bunleap <bunleap.sorn@gmail.com>
2021-04-07 19:46:04 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# KlemQuiz
+All of the files for KlemSampleQuiz.
+
+## Dev v0.1.0
+- [x] Quiz w/ Timer
+- [x] Quiz w/o Timer
+- [ ] Drag & Drop (Status: Err001Drop)
---
+++ src/js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+    numb: 1,
+    question: "តើបាតុភូតណាដែលកើត​មានក្នុងធម្មជាតិ បង្ហាញពីការផ្ទេរបន្ទុកអគ្គិសនី?",
+    answer: "ផ្លេកបន្ទោរ",
+    options: [
+      "បន្ទុះភ្នំភ្លើង",
+      "ផ្លេកបន្ទោរ",
+      "ភ្លៀងធ្លាក់",
+      "រញ្ជួយដី"
+    ]
+  },
+  {
+    numb: 2,
+    question: "តើយើងអាចបង្កើតបន្ទុកអគ្គិសនីយ៉ាងដូចម្ដេច?",
+    answer: "យើងអាចបង្កើតបន្ទុកអគ្គិសនីដោយត្រដុសវត្ថុ នឹងវត្ថុដែលមានប្រភេទខុសគ្នា។",
+    options: [
+      "យើងអាចបង្កើតបន្ទុកអគ្គិសនីដោយ ក្រវីវត្ថុ។",
+      "យើងទទួលបានពីប៉ាតឺរី។",
+      "យើងអាចបង្កើតបន្ទុកអគ្គិសនីដោយត្រដុសវត្ថុ នឹងវត្ថុដែលមានប្រភេទខុសគ្នា។",
+      "យើងអាចបង្កើតបន្ទុកអគ្គិសនីដោយត្រដុសវត្ថុ ជាមួយសំពត់សើម។"
+    ]
+  },
+  {
+    numb: 3,
+    question: "តើកកិតបង្កើត បន្ទុកអគ្គិសនីយ៉ាងដូចម្ដេច?",
+    answer: "ប្រសិនបើអ្នកខាត់អង្គធាតុមួយដោយអង្គធាតុមួយទៀតអេឡិចត្រុងត្រូវបានផ្ទេរពីអង្គធាតុមួយទៅអង្គធាតុមួយទៀត។",
+    options: [
+      "ប្រសិនបើអ្នកខាត់អង្គធាតុមួយដោយអង្គធាតុមួយទៀតអេឡិចត្រុងត្រូវបានផ្ទេរពីអង្គធាតុមួយទៅអង្គធាតុមួយទៀត។",
+      "កម្ដៅបង្កើតដោយកកិតនៃអង្គធាតុ បង្កើយអគ្គិសនី។",
+      "ថាមពលត្រដុសធ្វើឲ្យបាត់អេឡិចត្រុង។",
+      "អង្គធាតុទទួលបានអគ្គិសនីពីខ្យល់ដោយការត្រដុសជាមួយគ្នា។"
+    ]
+  },
+  // {
+  //   numb: 4,
+  //   question: "What does SQL stand for?",
+  //   answer: "Structured Query Language",
+  //   options: [
+  //     "Stylish Question Language",
+  //     "Stylesheet Query Language",
+  //     "Statement Question Language",
+  //     "Structured Query Language"
+  //   ]
+  // },
+  // {
+  //   numb: 5,
+  //   question: "What does XML stand for?",
+  //   answer: "eXtensible Markup Language",
+  //   options: [
+  //     "eXtensible Markup Language",
+  //     "eXecutable Multiple Language",
+  //     "eXTra Multi-Program Language",
+  //     "eXamine Multiple Language"
+  //   ]
+  // },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
